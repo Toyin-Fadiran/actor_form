6f530a3118e7183c786ed9cee2a52180593a8fc5
Toyin Fadiran <Oluwatoyin.Fadiran.Contractor@pepsico.com>
2024-05-30 23:44:40 -0500

    changes made locally pushed

--- src/app/actor-form/actor-form/actor-form.component.ts
+++ src/app/actor-form/actor-form/actor-form.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { Actor } from '../../actor';
 import { AnyARecord } from 'dns';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-actor-form',
   templateUrl: './actor-form.component.html',
   standalone: true, // Add this line to mark the component as standalone,
-  styleUrl: './actor-form.component.css'
+  styleUrl: './actor-form.component.css',
+  imports: [FormsModule, CommonModule],
 })
 export class ActorFormComponent {
 
@@ -33,6 +36,16 @@ heroine(): Actor {
 newActor() {
     this.model = new Actor(42, '', '');
 }
+
+  //////// NOT SHOWN IN DOCS ////////
+
+  // Reveal in html:
+  //   Name via form.controls = {{showFormControls(actorForm)}}
+  showFormControls(form: any) {
+    return form && form.controls.name && form.controls.name.value; // Tom Cruise
+  }
+
+  /////////////////////////////
 
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { ActorFormComponent } from './actor-form/actor-form/actor-form.component
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true, // Add this line to mark the component as standalone,
-  imports: [FormsModule, CommonModule, ActorFormComponent],
+  imports: [ActorFormComponent],
 })
 export class AppComponent {
   title = 'actor-form-app';
